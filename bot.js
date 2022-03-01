783a7ba1e95edd3e0c6dc67f3d50c171961b1f09
ArtemArt <71555709+ArtemGames06@users.noreply.github.com>
2022-03-01 21:09:26 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { Client, Intents } from 'discord.js';
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES] });
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.TOKEN);
