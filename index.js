1464dd2f7b37af2bb29e57da89a8d89c0626b17b
ArtemArt <71555709+ArtemGames06@users.noreply.github.com>
2022-03-01 21:23:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-import { Client, Intents } from 'discord.js';
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES] });
-
-client.on('ready', () => {
-	console.log(`Запустился бот ${client.user.tag}!`);
-});
-
-client.login(process.env.TOKEN);
+require = require('esm')(module);
+module.exports = require('./bot');
