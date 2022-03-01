835f09a156576920a08cc25a52966e405ba663a5
ArtemArt <71555709+ArtemGames06@users.noreply.github.com>
2022-03-01 21:06:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const { Client, Intents } = require('discord.js');
+import { Client, Intents } from 'discord.js';
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES] });
 
 client.on('ready', () => {
